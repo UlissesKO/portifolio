7760c1a984088f4db15299490aeb3048bbc19fa7
Ulisses <ulisseskelermann@gmail.com>
2024-06-05 19:41:36 -0300

    Acertei a estrutura das coisas

--- GerenciadorLinhas/func.py
+++ GerenciadorLinhas/func.py
@@ -2,8 +2,6 @@ import sqlite3 as sql
 from tkinter import *
 from tkinter import ttk
 
-WINDOW_WIDTH = 500
-WINDOW_HEIGHT = 700
 BUTTON_WIDTH = 15
 BACKGROUND_COLOR = "#b4dff0"
 FONT_COLOR = "#030608"
@@ -14,7 +12,7 @@ class Gerenciador():
     def __init__(self, path):
         self.path = path
         
-        self.banco = sql.connect("GerenciadorLinhas\linhas.sql")
+        self.banco = sql.connect(path)
         self.cursor = self.banco.cursor()
 
         #verifica se a tabela em questão existe
@@ -24,13 +22,18 @@ class Gerenciador():
         existe = self.cursor.fetchone() is not None
 
         if existe == False:
-            self.cursor.execute("CREATE TABLE cadastros (códigoLinha, corLinha, qntd)")
+            self.cursor.execute("CREATE TABLE linhas (códigoLinha, corLinha, qntd)")
             self.banco.commit()
+        else:
+            pass
     
-    def consultarBanco():
+    def consultarBanco():#Vai ver quantas linhas tem no banco, pra colocar no hud
         pass
 
-    def adicionarLinha():
+    def alterarQntdLinha():#Vai mudar a quantidade de linhas de tal cor no banco
+        pass
+
+    def adicionarCorNova():#Vai jogar uma nova cor de linha no banco de dados
         pass
     
 #Tkinter
@@ -42,3 +45,11 @@ def addLinha():
     cod = ttk.Entry(janela, font=TEXT_FONT, width=6, background="white")
     cod.grid(row=0, column=1)
 
+    ttk.Label(janela, text="Escreva a cor da linha", font=TEXT_FONT, foreground=FONT_COLOR, background=BACKGROUND_COLOR, padding=10).grid(row=1, column=0)
+    cor = ttk.Entry(janela, font=TEXT_FONT, width=6, background="white")
+    cor.grid(row=1, column=1)
+
+    add = ttk.Button(janela, text="Add", takefocus=False, padding=2, width=BUTTON_WIDTH)
+    add.grid(row=4, column=2)#Esse botao vai chamar a função adicionarCorNova()
+
+obj = Gerenciador("GerenciadorLinhas\linhas.sql")
--- GerenciadorLinhas/main.py
+++ GerenciadorLinhas/main.py
@@ -3,22 +3,10 @@ from tkinter import ttk
 import func
 
 
-#Tkinter começa aqui#####################################
 window = Tk()
 window.resizable(False, False)
 
-
-window.update()#Daqui
-window_width = window.winfo_width()
-window_height = window.winfo_height()
-screen_width = window.winfo_screenwidth()
-screen_height = window.winfo_screenheight()
-x = int((screen_width/2) - (window_width*2))
-y = int((screen_height/2) - (window_height*2))#até aqui é pra centralizar a janela na tela
-window.geometry(f"{func.WINDOW_WIDTH}x{func.WINDOW_HEIGHT}+{x}+{y}")#aqui ajusta o tamanho e seta a janela no meio da tela
-
-#Quadro principal
-frame = Frame(window, padx= 5, pady=5, background=func.BACKGROUND_COLOR, name="gerenciador", width=func.WINDOW_WIDTH, height=func.WINDOW_HEIGHT)
+frame = Frame(window, padx= 5, pady=5, background=func.BACKGROUND_COLOR, name="gerenciador")
 frame.grid(sticky="nswe")
 
 window.grid_rowconfigure(0, weight=1)
@@ -27,7 +15,9 @@ window.grid_columnconfigure(0, weight=1)
 title = ttk.Label(frame, text= "Gerenciador de linhas", font=func.TITLE_FONT, foreground=func.FONT_COLOR, background=func.BACKGROUND_COLOR, anchor="center", padding=10)
 title.grid(row=0, column=0, columnspan=8, sticky="nsew")
 
+#Vou precisar fazer algo parecido com for loop nas linhas do banco de dados pra colocar elas aqui
+
 add = ttk.Button(frame, text="Adicionar linha", takefocus=False, command=lambda:func.addLinha(), width=func.BUTTON_WIDTH)
-add.grid(row=1, column=8, sticky="nsew")
+add.grid(row=6, column=8, sticky="nsew")
 
 window.mainloop()
